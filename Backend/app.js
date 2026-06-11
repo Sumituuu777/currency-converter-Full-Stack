79c7a65cd5b8e1baddd576840d8061a21f9ad2c1
Sumit kumar <sumit.ydv.kumar2006@gmail.com>
2026-06-11 23:05:02 +0530

    deploy changes

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 const cors=require("cors")
-
+require('dotenv').config();
 
 //local modules
 const exchangeRouter=require("./routers/exchangeRouter")
